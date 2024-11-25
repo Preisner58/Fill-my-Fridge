512b4081f2d6e5651dab5a681f517a8d3fc0552f
Flávia Preisner <preisner58@gmail.com>
2024-11-25 15:17:42 -0300

    Update fillMyFridge.js

--- fillMyFridge.js
+++ fillMyFridge.js
@@ -4,5 +4,5 @@ const menu=document.querySelector(#menu);
 const btn_home=document.querySelector(#btn_home);
 const btn_novo=document.querySelector(#btn_novo);
 const btn_gestao=document.querySelector(#btn_gestao);
-const btn_sobre=document.querySelector(#btn_usuario);
+const btn_usuario=document.querySelector(#btn_usuario);
 const principal=document.querySelector(#principal);
