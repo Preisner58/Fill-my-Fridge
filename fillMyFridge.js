9397c97c03b65726043c3a25e70ebf91802a7c2b
Flávia Preisner <preisner58@gmail.com>
2025-05-12 21:04:24 -0300

    Delete fillMyFridge.js

--- fillMyFridge.js
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE = javascript>
-const cabecalho=document.querySelector(#cabecalho);
-const menu=document.querySelector(#menu);
-const btn_home=document.querySelector(#btn_home);
-const btn_novo=document.querySelector(#btn_novo);
-const btn_gestao=document.querySelector(#btn_gestao);
-const btn_usuario=document.querySelector(#btn_usuario);
-const principal=document.querySelector(#principal);
